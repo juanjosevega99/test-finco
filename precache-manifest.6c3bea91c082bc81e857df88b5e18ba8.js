68581341889368d5a515d4ef5803d716ec32cc40
Juna Jose Vega <juanjosevegaquintero@hotmail.com>
2020-09-07 17:06:59 -0500

    Updates

---
+++ precache-manifest.6c3bea91c082bc81e857df88b5e18ba8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1b83b338d0bffcaa163cbda063680071",
+    "url": "/test-finco/index.html"
+  },
+  {
+    "revision": "6e13169d3068a0e1d2e5",
+    "url": "/test-finco/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "fad0f4d91df704fc8cc5",
+    "url": "/test-finco/static/js/2.5cfdfd4e.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/test-finco/static/js/2.5cfdfd4e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6e13169d3068a0e1d2e5",
+    "url": "/test-finco/static/js/main.37daff76.chunk.js"
+  },
+  {
+    "revision": "1c5ee75dc852ba920402",
+    "url": "/test-finco/static/js/runtime-main.47a246aa.js"
+  }
+]);
